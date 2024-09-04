dc637fa781fb2d53336fbca98dadc45381a7d585
danielaorvzco <daniela12ledesma@gmail.com>
2024-09-03 22:24:50 -0600

    Agregar ejercicio resta

---
+++ Resta/Resta.java
@@ -0,0 +1,16 @@
+/* Progrmación Orientada a Objetos
+   Actividad 01: Hello World!
+   Alumno: Daniela Orozco Ledesma
+   Fecha: 29 de Agosto de 2024
+*/
+
+public class Resta {
+	
+	public static void main(String[] args){
+		int numero1 = 10;
+		int numero2 = 5;
+		System.out.print("El resultado de la resta es: " + (numero1 - numero2) );
+	}
+
+}
+
